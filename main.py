3145d2f3e58201d6a1af41efba87e3a4f2ed2293
gkyurkchiyski <g.kyurkchiyski@gmail.com>
2025-03-18 18:58:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import datetime
+
+target_datetime = datetime.datetime(2020, 1, 2, 12, 39, 1)
+current_datetime = datetime.datetime.now()
+
+if target_datetime < current_datetime:
+        print("Target date has passed")
+else:
+    print("Target date has NOT passed")
