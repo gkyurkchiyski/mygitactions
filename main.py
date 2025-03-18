9210187f3770d12785269263443c7788b4f454bb
gkyurkchiyski <g.kyurkchiyski@gmail.com>
2025-03-18 19:05:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ target_datetime = datetime.datetime(2020, 1, 2, 12, 39, 1)
 current_datetime = datetime.datetime.now()
 
 if target_datetime < current_datetime:
-        print("Target date has passed")
+    print("Target date has passed")
 else:
     print("Target date has NOT passed")
